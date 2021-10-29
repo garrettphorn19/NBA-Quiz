9fd948411b1f074f7cf299358950cad132d6278c
garrettphorn19 <garrett@sbfmail.com>
2021-10-29 12:43:39 -0400

    Made Function Modular

--- js/index.js
+++ js/index.js
@@ -1,3 +1,25 @@
+function getTop(teamData, listLength, stat) {
+    var all = []
+    var top = [];
+
+    for (var i=0; i<teamData.length; i++) {
+        var tempRoster = teamData[i]["roster"]
+        for (var j=0; j<tempRoster.length; j++) {
+            if (tempRoster[j]["points"] > 0) {
+                all.push(tempRoster[j]);
+                all.sort(function (a, b) {
+                    return b[stat] - a[stat];
+                });
+            }
+        }
+    }
+    for (var k=0; k < listLength; k++) {
+        top.push(all[k])
+    }
+
+    return top
+}
+
 function readTextFile(file, callback) {
     var rawFile = new XMLHttpRequest();
     rawFile.overrideMimeType("application/json");
@@ -13,28 +35,7 @@ function readTextFile(file, callback) {
 readTextFile("./json/nba_teams.json", function(text){
     var teamData = JSON.parse(text);
 
-    var topScorers = getTopScorers(teamData, 50)
-    console.log(topScorers)
+    var topScorers = getTop(teamData, 50, "points")
+    var topRebounders = getTop(teamData, 50, "rebounds")
+    var topAssistors = getTop(teamData, 50, "assists")
 });
-
-function getTopScorers(teamData, listLength) {
-    var allScorers = []
-    var topScorers = [];
-
-    for (var i=0; i<teamData.length; i++) {
-        var tempRoster = teamData[i]["roster"]
-        for (var j=0; j<tempRoster.length; j++) {
-            if (tempRoster[j]["points"] > 0) {
-                allScorers.push(tempRoster[j]);
-                allScorers.sort(function (a, b) {
-                    return b.points - a.points;
-                });
-            }
-        }
-    }
-    for (var k=0; k < listLength; k++) {
-        topScorers.push(allScorers[k])
-    }
-
-    return topScorers
-}
