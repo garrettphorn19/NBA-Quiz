85367a47336f21be41e58a74775f1253efe0622a
garrettphorn19 <garrett@sbfmail.com>
2021-10-29 12:38:15 -0400

    Top Scorers Added

--- js/index.js
+++ js/index.js
@@ -13,7 +13,28 @@ function readTextFile(file, callback) {
 readTextFile("./json/nba_teams.json", function(text){
     var teamData = JSON.parse(text);
 
-    for (var i=0; i<teamData.length; i++) {
-        console.log(teamData[i])
-    }
+    var topScorers = getTopScorers(teamData, 50)
+    console.log(topScorers)
 });
+
+function getTopScorers(teamData, listLength) {
+    var allScorers = []
+    var topScorers = [];
+
+    for (var i=0; i<teamData.length; i++) {
+        var tempRoster = teamData[i]["roster"]
+        for (var j=0; j<tempRoster.length; j++) {
+            if (tempRoster[j]["points"] > 0) {
+                allScorers.push(tempRoster[j]);
+                allScorers.sort(function (a, b) {
+                    return b.points - a.points;
+                });
+            }
+        }
+    }
+    for (var k=0; k < listLength; k++) {
+        topScorers.push(allScorers[k])
+    }
+
+    return topScorers
+}
